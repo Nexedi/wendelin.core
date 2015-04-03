e8cea9338b0f85b2c186098809abff1f72a593ba
Kirill Smelkov <kirr@nexedi.com>
2015-04-03 11:29:22 +0300

    wendelin/list.h: include list.h from linux with needed headers pre-included

---
+++ include/wendelin/list.h
@@ -0,0 +1,16 @@
+#ifndef _WENDELIN_LIST_H_
+#define _WENDELIN_LIST_H_
+
+/* Double-linked lists
+ * No copyright is claimed
+ */
+
+/* linux/list.h requires the following */
+#include <stddef.h>
+#include <string.h>
+#include <ccan/array_size/array_size.h>
+
+#include <linux/list.h>
+
+
+#endif
