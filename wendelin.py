e870781d4c030e2eb7022ee72d57985697d34fb4
Kirill Smelkov <kirr@nexedi.com>
2015-04-03 09:40:22 +0300

    Top-level in-tree import redirector
    For working copy checkout we would like to work on the source
    right-away, without putting it under additional wendelin/ (and possibly
    /core/) subdirectories.
    
    Add in-tree modules redirector which tweaks plain-python and
    pkg_resources-based imports so that e.g.
    
        import wendelin.bigarray
    
    while in-tree resolves to
    
        bigarray/__init__.py
    
    In installed state, there will be no such redirector, and code will be
    installed with conventional structure.

--- wendelin.py
+++ wendelin.py
@@ -1,3 +1,4 @@
+# Wendelin.core | Top-level in-tree python import redirector
 # Copyright (C) 2014-2015  Nexedi SA and Contributors.
 #                          Kirill Smelkov <kirr@nexedi.com>
 #
@@ -14,3 +15,39 @@
 # warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 #
 # See COPYING file for full licensing terms.
+
+
+# tell python wendelin.* modules hierarchy starts at top-level
+#
+# This allows e.g.          `import wendelin.bigarray`
+# to resolve to importing   `bigarray/__init__.py`
+#
+# and thus avoid putting everything in additional top-level wendelin/
+# directory in source tree.
+#
+# see https://www.python.org/doc/essays/packages/ about __path__
+# XXX avoid being imported twice, e.g. `import wendelin.wendelin` should not work.
+
+from os.path import dirname, realpath
+__path__ = [realpath(dirname(__file__))]
+del dirname, realpath
+
+
+# Also tell setuptools/pkg_resources 'wendelin' is a namespace package
+# ( so that wendelin.core installed in development mode does not brake
+#   'wendelin' namespacing wrt other wendelin software )
+__import__('pkg_resources').declare_namespace(__name__)
+
+# pkg_resources will append '.../wendelin' to __path__ which is not right for
+# in-tree setup and thus is not needed here. Remove it.
+del __path__[1:]
+
+
+# in the end we have:
+# __path__ has 1 item
+# __path__[0] points to working tree
+# __name__ registered as namespace package
+#
+# so the following should work:
+#   - importing from in-tree files
+#   - importing from other children of wendelin packages
