c507d9009f59fec2041bac9c31c5b08a48d3897d
Kirill Smelkov <kirr@nexedi.com>
2015-10-02 16:02:34 +0300

    wendelin.core v0.5

--- setup.py
+++ setup.py
@@ -206,7 +206,7 @@ def readfile(path):
 
 setup(
     name        = 'wendelin.core',
-    version     = '0.4',
+    version     = '0.5',
     description = 'Out-of-core NumPy arrays',
     long_description = '%s\n----\n\n%s' % (
                             readfile('README.rst'), readfile('CHANGELOG')),
