Nexedi/wendelin.core

commit 450ad804625080d69b646126d1e015abe98889b7

bigarray: ArrayRef support for BigArray
Rationale
---------

Array reference could be useful in situations where one needs to pass arrays
between processes and instead of copying array data, leverage the fact that
top-level array, for example ZBigArray, is already persisted separately, and
only send small amount of information referencing data in question.

Implementation
--------------

BigArray is not regular NumPy array and so needs explicit support in
ArrayRef code to find root object and indices. This patch adds such
support via the following way:

- when BigArray.__getitem__ creates VMA, it remembers in the VMA
  the top-level BigArray object under which this VMA was created.

- when ArrayRef is finding root, it can detect such VMAs, because it will
  be pointed to by the most top regular ndarray's .base, and in turn gets
  top-level BigArray object from the VMA.

- further all indices computations are performed, similarly to complete regular
  ndarrays case, on ndarrays root and a. But in the end .lo and .hi are
  adjusted for the corresponding offset of where root is inside whole
  BigArray.

- there is no need to adjust .deref() at all.

For remembering information into a VMA and also to be able to get
(readonly) its mapping addresses _bigfile.c extension has to be extended
a bit. Since we are now storing arbitrary python object attached to
PyVMA - it can create cycles - and so PyVMA accordingly adjusted to
support cyclic garbage collector.

Please see the patch itself for more details and comments.

bigfile/_bigfile.c
@@ -1,5 +1,5 @@
 /* Wendelin.bigfile | Python interface to memory/files
- * Copyright (C) 2014-2015  Nexedi SA and Contributors.
+ * Copyright (C) 2014-2018  Nexedi SA and Contributors.
  *                          Kirill Smelkov <kirr@nexedi.com>
  *
  * This program is free software: you can Use, Study, Modify and Redistribute
@@ -59,12 +59,36 @@ static PyObject *pybuf_str;
 
 /*
  * python representation of VMA - exposes vma memory as python buffer
+ *
+ * also exposes:
+ *
+ *      .filerange()            to know which range in mmaped file this vma covers.
+ *      .pagesize()             to know page size of underlying RAM.
+ *
+ * and:
+ *
+ *      .addr_start, .addr_stop to know offset of ndarray in VMA.
+ *      .pyuser                 generic python-level attribute (see below).
  */
 struct PyVMA {
     PyObject;
     PyObject *in_weakreflist;
 
     VMA;
+
+    /* python-level user of this VMA.
+     *
+     * for example for ArrayRef to work, BigArray needs to find out VMA ->
+     * top-level BigArray object for which this VMA was created.
+     *
+     * There is vma -> fileh -> file chain, but e.g. for a given ZBigFile there
+     * can be several ZBigArrays created on top of it to view its data (e.g. via
+     * BigArray.view()). So even if it can go from vma to -> zfile it does not
+     * help to find out the top-level ZBigArray object itself.
+     *
+     * This way we allow BigArray python code to set vma.pyuser attribute
+     * pointing to original BigArray object for which this VMA was created. */
+    PyObject *pyuser;
 };
 typedef struct PyVMA PyVMA;
 
@@ -140,6 +164,11 @@ void XPyBufferObject_Unpin(PyBufferObject *bufo);
 void XPyBuffer_Unpin(Py_buffer *view);
 
 
+#define PyFunc(FUNC, DOC)               \
+static const char FUNC ##_doc[] = DOC;  \
+static PyObject *FUNC
+
+
 /************
  *  PyVMA   *
  ************/
@@ -193,6 +222,50 @@ pyvma_len(PyObject *pyvma0)
 }
 
 
+/* pyvma vs cyclic GC */
+static int
+pyvma_traverse(PyObject *pyvma0, visitproc visit, void *arg)
+{
+    PyVMA *pyvma = upcast(PyVMA *, pyvma0);
+
+    Py_VISIT(pyvma->pyuser);
+    return 0;
+}
+
+static int
+pyvma_clear(PyObject *pyvma0)
+{
+    PyVMA *pyvma = upcast(PyVMA *, pyvma0);
+
+    Py_CLEAR(pyvma->pyuser);
+    return 0;
+}
+
+
+PyFunc(pyvma_filerange, "filerange() -> (pgoffset, pglen) -- file range this vma covers")
+    (PyObject *pyvma0, PyObject *args)
+{
+    PyVMA *pyvma = upcast(PyVMA *, pyvma0);
+    Py_ssize_t pgoffset, pglen;     // XXX Py_ssize_t vs pgoff_t
+
+    pgoffset = pyvma->f_pgoffset;
+    pglen    = (pyvma->addr_stop - pyvma->addr_start) / pyvma->fileh->ramh->ram->pagesize;
+    /* NOTE ^^^ addr_stop and addr_start must be page-aligned */
+
+    return Py_BuildValue("(nn)", pgoffset, pglen);
+}
+
+
+PyFunc(pyvma_pagesize, "pagesize() -> pagesize -- page size of RAM underlying this VMA")
+    (PyObject *pyvma0, PyObject *args)
+{
+    PyVMA *pyvma = upcast(PyVMA *, pyvma0);
+    Py_ssize_t pagesize = pyvma->fileh->ramh->ram->pagesize;
+
+    return Py_BuildValue("n", pagesize);
+}
+
+
 static void
 pyvma_dealloc(PyObject *pyvma0)
 {
@@ -210,6 +283,7 @@ pyvma_dealloc(PyObject *pyvma0)
         Py_DECREF(pyfileh);
     }
 
+    pyvma_clear(pyvma);
     pyvma->ob_type->tp_free(pyvma);
 }
 
@@ -247,12 +321,35 @@ static /*const*/ PySequenceMethods pyvma_as_seq = {
 };
 
 
+static /*const*/ PyMethodDef pyvma_methods[] = {
+    {"filerange",   pyvma_filerange,    METH_VARARGS,   pyvma_filerange_doc},
+    {"pagesize",    pyvma_pagesize,     METH_VARARGS,   pyvma_pagesize_doc},
+    {NULL}
+};
+
+// XXX vvv better switch on various possibilities and find approptiate type
+// (e.g. on X32 uintptr_t will be 4 while long will be 8)
+const int _ =
+    BUILD_ASSERT_OR_ZERO(sizeof(uintptr_t) == sizeof(unsigned long));
+#define T_UINTPTR   T_ULONG
+
+static /*const*/ PyMemberDef pyvma_members[] = {
+    {"addr_start",  T_UINTPTR,      offsetof(PyVMA, addr_start),  READONLY, "vma's start addr"},
+    {"addr_stop",   T_UINTPTR,      offsetof(PyVMA, addr_stop),   READONLY, "vma's start addr"},
+    // XXX pyuser: restrict to read-only access?
+    {"pyuser",      T_OBJECT_EX,    offsetof(PyVMA, pyuser),      0,        "user of this vma"},
+    {NULL}
+};
+
 static PyTypeObject PyVMA_Type = {
     PyVarObject_HEAD_INIT(NULL, 0)
     .tp_name            = "_bigfile.VMA",
     .tp_basicsize       = sizeof(PyVMA),
-    .tp_flags           = Py_TPFLAGS_DEFAULT | Py_TPFLAGS_HAVE_NEWBUFFER,
-    .tp_methods         = NULL, // TODO ?
+    .tp_flags           = Py_TPFLAGS_DEFAULT | Py_TPFLAGS_HAVE_NEWBUFFER | Py_TPFLAGS_HAVE_GC,
+    .tp_traverse        = pyvma_traverse,
+    .tp_clear           = pyvma_clear,
+    .tp_methods         = pyvma_methods,
+    .tp_members         = pyvma_members,
     .tp_as_sequence     = &pyvma_as_seq,
     .tp_as_buffer       = &pyvma_as_buffer,
     .tp_dealloc         = pyvma_dealloc,
@@ -267,10 +364,6 @@ static PyTypeObject PyVMA_Type = {
  *  PyBigFileH  *
  ****************/
 
-
-#define PyFunc(FUNC, DOC)               \
-static const char FUNC ##_doc[] = DOC;  \
-static PyObject *FUNC
 
 PyFunc(pyfileh_mmap, "mmap(pgoffset, pglen) - map fileh part into memory")
     (PyObject *pyfileh0, PyObject *args)

bigarray/tests/test_basic.py
@@ -679,7 +679,17 @@ def test_arrayref():
     assert array_equal(ref.deref(), a)
 
 
-    for root in (data, rdata):
+    # BigArray with data backend.
+    # data_ is the same as data but shifted to exercise vma and vma->broot offsets calculation.
+    data_ = zeros(8*PS, dtype=uint8)
+    data_[2*PS-1:][:PS] = data
+    f  = BigFile_Data_RO(data_, PS)
+    fh = f.fileh_open()
+    A  = BigArray(data_.shape, data_.dtype, fh)
+    assert array_equal(A[2*PS-1:][:PS], data)
+
+
+    for root in (data, rdata, A):  # both ndarray and BigArray roots
         # refok verifies whether ArrayRef(x) works ok
         def refok(x):
             ref = ArrayRef(x)
@@ -702,7 +712,10 @@ def test_arrayref():
             assert array_equal(ref.deref(), x)
 
 
-        if root is rdata:
+        if isinstance(root, BigArray):
+            a = root[2*PS-1:][:PS]      # get to `data` range
+        # typeof(root) = ndarray
+        elif root is rdata:
             a = root[::-1]              # rdata
         else:
             a = root[:]                 # data

bigarray/__init__.py
@@ -410,6 +410,10 @@ class BigArray(object):
             # ~~~ mmap file part corresponding to full major slice into memory
             vmaM = self._fileh.mmap(pageM_min, pageM_max-pageM_min+1)
 
+            # remember to which BigArray this vma belongs.
+            # this is needed for ArrayRef to be able to find root array object.
+            vmaM.pyuser = self
+
 
             # first get ndarray view with only major slice specified and rest indices being ":"
             viewM_shape   = Mreplace(self._shape, nitemsM)
@@ -500,9 +504,29 @@ def _flatbytev(a):
 # The reference is represented by root array object and instructions how to
 # create original array as some view of the root.
 #
+# Such reference could be useful in situations where one needs to pass arrays
+# between processes and instead of copying array data, leverage the fact that
+# top-level array, for example ZBigArray, is already persisted separately, and
+# only send small amount of information referencing data in question.
+#
 # Use ArrayRef(array) to create reference to an ndarray.
 #
 # Use .deref() to convert ArrayRef to pointed array object.
+#
+# NOTE
+#
+# don't send ArrayRef unconditionally - for example when array object is
+# small regular ndarray with also regular, but big, root ndarray, sending
+# ArrayRef will send whole data for root object, not for small leaf.
+#
+# Sending ArrayRef only makes sense when root object is known to be already
+# persisted by other means, for example something like below in ZODB context:
+#
+#   aref = ArrayRef(a)
+#   if isinstance(aref.root, Persistent):
+#       send aref
+#   else:
+#       send a
 class ArrayRef(object):
     # .root         top-level array object
     #
@@ -629,6 +653,7 @@ class ArrayRef(object):
     def __init__(aref, a):
         # find root
         root = a            # top-level ndarray
+        bigvma = None       # VMA, that is root.base, if there is one
         while 1:
             base = root.base
 
@@ -650,7 +675,9 @@ class ArrayRef(object):
 
             # base is neither ndarray (sub)class nor ndarray proxy.
             #
-            # it is top-level ndarray with base taken from an object
+            # either it is
+            #
+            # 1) top-level ndarray with base taken from an object
             # with buffer interface, e.g. as here:
             #
             #   In [1]: s = '123'
@@ -660,7 +687,14 @@ class ArrayRef(object):
             #   In [4]: x.base
             #   Out[4]: '123'
             #
-            # and so it should be treated as top-level ndarray.
+            # and so it should be treated as top-level ndarray,
+            #
+            # 2) or it is a VMA created from under BigArray which will be
+            # treated as top-level too, and corrected for in the end.
+            basetype = type(base)
+            if basetype.__module__ + "." + basetype.__name__ == "_bigfile.VMA":
+            #if isinstance(base, _bigfile.VMA):  XXX _bigfile does not expose VMA
+                bigvma = base
             break
 
 
@@ -715,6 +749,23 @@ class ArrayRef(object):
         aref.stridev = a.strides
         aref.dtype   = a.dtype
         aref.atype   = type(a)
+
+        # correct it, if the root is actually BigArray
+        if bigvma is not None:
+            assert bigvma.addr_start <= rdata[0]
+            assert rdata[0] + len(broot) <= bigvma.addr_stop
+
+            bigroot = bigvma.pyuser
+            assert isinstance(bigroot, BigArray)
+
+            # bigoff is broot position in bbigroot (both raw flat []byte ↑ along memory)
+            pgoff, _ = bigvma.filerange()
+            bigoff = pgoff * bigvma.pagesize()          # vma start offset
+            bigoff += rdata[0] - bigvma.addr_start      # broot offset from vma start
+
+            aref.root  = bigroot
+            aref.lo   += bigoff
+            aref.hi   += bigoff
 
         # we are done
         return

commit ae8a4b8d6fae0b48b488cb869932a085d0a4633c

bigfile/py: Properly untrack PyVMA from GC before dealloc
On a testing instance we started to see segfaults in pyvma_dealloc()
with inside calls to vma_unmap but with NULL pyvma->fileh. That was
strange, becuse before calling vma_unmap(), the code explicitly checks
whether pyvma->fileh is !NULL.

That was, as it turned out, due to pyvma_dealloc being called twice at the
same time from two python threads. Here is how that was possible:

T1 decrefs pyvma and finds its reference count drops to zero. It calls
pyvma_dealloc. From there vma_unmap() is called, which calls virt_lock()
and that releases GIL first. Another thread T2 was waiting for GIL, it
acquires it, does some work at python level and somehow triggers GC.
Since PyVMA supports cyclic GC, it was on GC list and thus GC calls
dealloc for the same vma again. Here is how it look in the backtraces:

T1:

	#0  0x00007f6aefc57827 in futex_abstimed_wait_cancelable (private=0, abstime=0x0, expected=0, futex_word=0x1e011d0) at ../sysdeps/unix/sysv/linux/futex-internal.h:205
	#1  do_futex_wait (sem=sem@entry=0x1e011d0, abstime=0x0) at sem_waitcommon.c:111
	#2  0x00007f6aefc578d4 in __new_sem_wait_slow (sem=0x1e011d0, abstime=0x0) at sem_waitcommon.c:181
	#3  0x00007f6aefc5797a in __new_sem_wait (sem=<optimized out>) at sem_wait.c:29
	#4  0x00000000004ffbc4 in PyThread_acquire_lock ()
	#5  0x00000000004dbe8a in PyEval_RestoreThread ()
	#6  0x00007f6ac6d3b8fc in py_gil_retake_if_waslocked (arg=0x4f18f00) at bigfile/_bigfile.c:1048
	#7  0x00007f6ac6d3dcfc in virt_gil_retake_if_waslocked (gilstate=0x4f18f00) at bigfile/virtmem.c:78
	#8  0x00007f6ac6d3dd30 in virt_lock () at bigfile/virtmem.c:92
	#9  0x00007f6ac6d3e724 in vma_unmap (vma=0x7f6a7e0c4100) at bigfile/virtmem.c:271
	#10 0x00007f6ac6d3a0bc in pyvma_dealloc (pyvma0=0x7f6a7e0c40e0) at bigfile/_bigfile.c:284
	...
	#13 0x00000000004d76b0 in PyEval_EvalFrameEx ()

T2:

	#5  0x00007f6ac6d3a081 in pyvma_dealloc (pyvma0=0x7f6a7e0c40e0) at bigfile/_bigfile.c:276
	#6  0x0000000000500450 in ?? ()
	#7  0x00000000004ffd82 in _PyObject_GC_New ()
	#8  0x0000000000485392 in PyList_New ()
	#9  0x00000000004d3bff in PyEval_EvalFrameEx ()

T2 does the work of vma_unmap and clears C-level vma. Then when T1 wakes up and
returns to vma_unmap, it sees vma->file and all other fields cleared -> oops
segfault.

Fix it by removing pyvma from GC list before going to do actual destruction.
This way if a concurrent GC triggers, it won't see the vma object on its list,
and thus won't have a chance to invoke its destructor the second time.

The bug was introduced in 450ad804 (bigarray: ArrayRef support for BigArray)

bigfile/_bigfile.c
@@ -1,5 +1,5 @@
 /* Wendelin.bigfile | Python interface to memory/files
- * Copyright (C) 2014-2018  Nexedi SA and Contributors.
+ * Copyright (C) 2014-2019  Nexedi SA and Contributors.
  *                          Kirill Smelkov <kirr@nexedi.com>
  *
  * This program is free software: you can Use, Study, Modify and Redistribute
@@ -26,6 +26,13 @@
  *   VMA with a buffer/memoryview interface
  *   BigFileH  with mmap (-> vma) and writeout control
  *   BigFile   base class (to allow implementing BigFile backends in python)
+ *
+ * NOTE virtmem/bigfile functions release/reacquire GIL (see virt_lock()) -
+ * thus functions that use them cannot assume they run mutually exclusive to
+ * other Python threads. See Py_CLEAR documentation which explain what could go
+ * wrong if code is not careful to protect itself against concurrent GC:
+ *
+ * https://github.com/python/cpython/blob/v2.7.15-310-g112e4afd582/Include/object.h#L790-L798
  */
 
 #include <Python.h>
@@ -269,6 +276,13 @@ PyFunc(pyvma_pagesize, "pagesize() -> pagesize -- page size of RAM underlying th
 static void
 pyvma_dealloc(PyObject *pyvma0)
 {
+    /* PyVMA supports cyclic GC - first, before destructing, remove it from GC
+     * list to avoid segfaulting on double entry here - e.g. if GC is triggered
+     * from a weakref callback, or concurrently from another thread.
+     *
+     * See https://bugs.python.org/issue31095 for details */
+    PyObject_GC_UnTrack(pyvma0);
+
     PyVMA       *pyvma = upcast(PyVMA *, pyvma0);
     BigFileH    *fileh = pyvma->fileh;
 
@@ -460,6 +474,8 @@ PyFunc(pyfileh_invalidate_page, "invalidate_page(pgoffset) - invalidate fileh pa
 static void
 pyfileh_dealloc(PyObject *pyfileh0)
 {
+    /* PyBigFileH does not support cyclic GC - no need to PyObject_GC_UnTrack it */
+
     PyBigFileH  *pyfileh = upcast(PyBigFileH *, pyfileh0);
     BigFile     *file = pyfileh->file;
     PyBigFile   *pyfile;

bigfile/tests/test_basic.py
@@ -413,6 +413,17 @@ def test_gc_from_sighandler():
     assert f3.marker_list == [2]
 
 
+# test that vma dealloc is safe wrt concurrent GC.
+def test_vma_del_vs_gc():
+    f   = XBigFile(b'abcd', PS)
+    fh  = f.fileh_open()
+    vma = fh.mmap(0, 1)
+
+    # del vma, but make sure vma.dealloc calls another code, which eventually calls GC.
+    # this will segfault, if vma.dealloc does not properly untrack vma from GC first.
+    wvma = weakref.ref(vma, lambda _: gc.collect())
+    del vma
+
 # test that there is no crash after first store error on writeout
 class StoreError(Exception):
     pass