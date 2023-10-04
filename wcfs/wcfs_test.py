ab6408715b885cd1c9aba15777f95efcaa9fd626
Levin Zimmermann <levin.zimmermann@nexedi.com>
2023-10-04 15:59:18 +0000

    wcfs_test: Add TODO - extend tests wrt faulty client
    In its current state we miss at least two test cases, to ensure the
    protection against faulty clients is fully functional:
    
    1. We need a test to cover a faulty client at 'readPinWatchers':
       currently we only check if faulty clients are killed which
       are already faulty at the initial watch request.
    
    2. We need to test if a sane client get really the data served
       in combination with a faulty client that's blocking.
    
    This patch adds TODOs for these two extensions.

--- wcfs/wcfs_test.py
+++ wcfs/wcfs_test.py
@@ -1449,6 +1449,10 @@ def test_wcfs_watch_going_back():
     wl.close()
 
 
+# TODO extend tests to also cover situation that a non-faulty
+# client continues to be served ok
+# TODO explicitly cover readPinWatchers behaviour with tests
+
 # verify that wcfs kills slow/faulty client who does not reply to pin in time.
 # protection against faulty/slow clients
 @func
