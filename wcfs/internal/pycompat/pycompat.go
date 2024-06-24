5a2b639cd6f88e73182e4037cec2e02b4046525c
Carlos Ramos Carreño <carlos.ramos@nexedi.com>
2024-06-24 15:05:33 +0200

    Parse `bytes` as str in Golang.
    [In Go, strings can contain arbitrary bytes](https://go.dev/blog/strings)
    (similar to the ones in Python 2), and there is no explicit `bytes`
    class.
    Thus, all the code that deals with arbitrary data in Go is currently
    using the string type.
    
    However, the [ogórek package](https://github.com/kisielk/og-rek) used
    to load Python pickles in Go, decodes Python 3 `bytes` objects as a
    special string type `ogórek.Bytes`, incompatible with APIs expecting
    normal strings.
    This causes errors such as the following:
    ```
    --- FAIL: TestΔFtail (1.31s)
    panic: @03f97a3b982b7bcc: get blkdata from obj 0000000000000002: ZBlk0(0000000000000002): loadBlkData: wendelin.bigfile.file_zodb.ZBlk0(0000000000000002): activate: pysetstate: expect str; got ogórek.Bytes [recovered]
            panic: file:///tmp/TestΔFtail493253177/001/1.fs: @03f97a3b982b7bcc: get blktab: @03f97a3b982b7bcc: get blkdata from obj 0000000000000002: ZBlk0(0000000000000002): loadBlkData: wendelin.bigfile.file_zodb.ZBlk0(0000000000000002): activate: pysetstate: expect str; got ogórek.Bytes [recovered]
            panic: file:///tmp/TestΔFtail493253177/001/1.fs: @03f97a3b982b7bcc: get blktab: @03f97a3b982b7bcc: get blkdata from obj 0000000000000002: ZBlk0(0000000000000002): loadBlkData: wendelin.bigfile.file_zodb.ZBlk0(0000000000000002): activate: pysetstate: expect str; got ogórek.Bytes
    ```
    
    In order to fix that, we have copied the [`Xstrbytes` function from NEO](
    https://lab.nexedi.com/kirr/neo/-/blob/f7776fc1689b0d62e582b132ecc017ab72dc3b23/go/zodb/internal/pickletools/pickletools.go#L49-64)
    in the `pycompat.go` file, which accepts either a string or an
    `ogórek.Bytes` object and returns the corresponding string.
    
    We have used that function whenever a `bytes` object could be present.

--- wcfs/internal/pycompat/pycompat.go
+++ wcfs/internal/pycompat/pycompat.go
@@ -3,7 +3,9 @@
 package pycompat
 
 import (
+	"fmt"
 	"math/big"
+	pickle "github.com/kisielk/og-rek"
 )
 
 // Int64 tries to convert unpickled Python value to int64.
@@ -12,14 +14,31 @@ import (
 //
 // XXX + support for float?
 func Int64(xv interface{}) (v int64, ok bool) {
-        switch v := xv.(type) {
-        case int64:
-                return v, true
-        case *big.Int:
-                if v.IsInt64() {
-                        return v.Int64(), true
-                }
-        }
+  switch v := xv.(type) {
+  case int64:
+    return v, true
+  case *big.Int:
+    if v.IsInt64() {
+      return v.Int64(), true
+    }
+  }
 
-        return 0, false
+  return 0, false
+}
+
+// Xstrbytes verifies and extacts str|bytes from unpickled value.
+func Xstrbytes(x interface{}) (string, error) {
+	var s string
+	switch x := x.(type) {
+	default:
+		return "", fmt.Errorf("expect str|bytes; got %T", x)
+
+	case string:
+		s = x
+
+	case pickle.Bytes:
+		s = string(x)
+	}
+
+	return s, nil
 }
