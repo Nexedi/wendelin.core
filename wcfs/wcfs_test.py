Nexedi/wendelin.core

commit e51bef0d26ebaa32d81f7a92370d3f3f86521e6a

wcfs: py: Log with date and time present
So that we can correlate py log with go log outputs which is important
to analyze things in case of a trouble.

Standard py logging has support only for millisecond-level precision
(.msecs attribute of log record), but millisecond is not enough a
precision to reconstruct flow of FUSE events of what was going on. So we
need to increase this to microsecond precision manually and to match
timestamps precisions emitted by wcfs.go .

For consistency with wcfs.go also adjust wcfs.py log format to match
that of wcfs.go because both those py and log levels constitute one
program and log simultaneously.

Before this patch wcfs.py log output was like this:

    WARNING:wcfs:fuse_unmount /dev/shm/wcfs/a027435914736c5128c7f5c4931a7a59567a9e86: ...

After the patch it looks like this:

    W0421 14:16:11.101631 3857246 __init__.py:582] wcfs: fuse_unmount /dev/shm/wcfs/a027435914736c5128c7f5c4931a7a59567a9e86: ...

/reviewed-by @levin.zimmermann
/reviewed-on https://lab.nexedi.com/nexedi/wendelin.core/-/merge_requests/36

setup.py
@@ -1,5 +1,5 @@
 # Wendelin.core | pythonic package setup
-# Copyright (C) 2014-2022  Nexedi SA and Contributors.
+# Copyright (C) 2014-2025  Nexedi SA and Contributors.
 #                          Kirill Smelkov <kirr@nexedi.com>
 #
 # This program is free software: you can Use, Study, Modify and Redistribute
@@ -378,6 +378,9 @@ setup(
 
                     Extension('wendelin.wcfs.internal.mm',
                         ['wcfs/internal/mm.pyx']),
+
+                    Extension('wendelin.wcfs.internal._os',
+                        ['wcfs/internal/_os.pyx']),
                   ],
 
     package_dir = {'wendelin': ''},

wcfs/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright (C) 2018-2024  Nexedi SA and Contributors.
+# Copyright (C) 2018-2025  Nexedi SA and Contributors.
 #                          Kirill Smelkov <kirr@nexedi.com>
 #
 # This program is free software: you can Use, Study, Modify and Redistribute
@@ -81,6 +81,7 @@ from persistent import Persistent
 from zodbtools.util import ashex as h
 
 from wendelin.lib.zodb import zurl_normalize_main
+from wendelin.wcfs.internal import glog
 from wendelin.wcfs.client._wcfs import \
     PyWCFS          as _WCFS,       \
     PyWatchLink     as WatchLink    \
@@ -735,7 +736,8 @@ def main():
 
     # setup log.warn/error to go to stderr, so that details could be seen on
     # e.g. "fuse_unmount: ... failed (more details logged)"
-    logging.basicConfig(stream=sys.stderr, level=logging.WARNING)
+    # tune logging to use the same glog output format as on wcfs.go side.
+    glog.basicConfig(stream=sys.stderr, level=logging.WARNING)
 
     if cmd == "serve":
         if argv[0] == '-h':

wcfs/internal/.gitignore
@@ -1,3 +1,4 @@
 /io.c
 /mm.c
+/_os.c
 /wcfs_test.cpp

wcfs/internal/_os.pyx
@@ -1,0 +1,32 @@
+# Copyright (C) 2024-2025  Nexedi SA and Contributors.
+#                          Kirill Smelkov <kirr@nexedi.com>
+#
+# This program is free software: you can Use, Study, Modify and Redistribute
+# it under the terms of the GNU General Public License version 3, or (at your
+# option) any later version, as published by the Free Software Foundation.
+#
+# You can also Link and Combine this program with other software covered by
+# the terms of any of the Free Software licenses or any of the Open Source
+# Initiative approved licenses and Convey the resulting work. Corresponding
+# source of such a combination shall include the source code for all other
+# software used.
+#
+# This program is distributed WITHOUT ANY WARRANTY; without even the implied
+# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+# See COPYING file for full licensing terms.
+# See https://www.nexedi.com/licensing for rationale and options.
+
+# cython: language_level=2
+
+"""Package wcfs.internal._os provides C-level part of package wcfs.internal.os ."""
+
+from posix.types cimport pid_t
+cdef extern from "<unistd.h>":
+    pid_t c_gettid "gettid"()
+
+# gettid returns ID of current thread.
+#
+# It is similar to _thread.get_native_id() which is available only on py3.
+def gettid():
+    return c_gettid()

wcfs/internal/glog.py
@@ -1,0 +1,70 @@
+# Copyright (C) 2024-2025  Nexedi SA and Contributors.
+#                          Kirill Smelkov <kirr@nexedi.com>
+#
+# This program is free software: you can Use, Study, Modify and Redistribute
+# it under the terms of the GNU General Public License version 3, or (at your
+# option) any later version, as published by the Free Software Foundation.
+#
+# You can also Link and Combine this program with other software covered by
+# the terms of any of the Free Software licenses or any of the Open Source
+# Initiative approved licenses and Convey the resulting work. Corresponding
+# source of such a combination shall include the source code for all other
+# software used.
+#
+# This program is distributed WITHOUT ANY WARRANTY; without even the implied
+# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+# See COPYING file for full licensing terms.
+# See https://www.nexedi.com/licensing for rationale and options.
+
+"""Package glog provides glog-style logging for Python.
+
+It merely wraps and adjusts standard package logging to do so:
+
+- use glog.basicConfig to setup logging
+- use standard logging.* functions do emit log messages
+"""
+
+from __future__ import print_function, absolute_import
+
+import logging, time
+from wendelin.wcfs.internal import os as xos
+
+
+# basicConfig is like logging.basicConfig but configures log output format to match glog.
+def basicConfig(stream, level):
+    logging.setLogRecordFactory(LogRecord)
+    h = logging.StreamHandler(stream)
+    f = Formatter()
+    h.setFormatter(f)
+    logging.root.addHandler(h)
+
+
+# LogRecord amends logging.LogRecord to also include .thread_id attribute.
+class LogRecord(logging.LogRecord):
+    def __init__(self, *argv, **kw):
+        logging.LogRecord.__init__(self, *argv, **kw)
+        self.thread_id = xos.gettid()
+
+
+# Formatter adjusts logging.Formatter to behave similarly to glog.
+#
+# it depends on LogRecord to be used instead of logging.LogRecord as the log record factory.
+class Formatter(logging.Formatter):
+    def __init__(self):
+        logging.Formatter.__init__(self,
+            "%(levelchar)s%(asctime)s %(thread_id)d %(filename)s:%(lineno)d] %(name)s: %(message)s",
+            "%m%d %H:%M:%S")
+
+    # provide .levelchar to record
+    def formatMessage(self, record):
+        record.levelchar = record.levelname[0]
+        return logging.Formatter.formatMessage(self, record)
+
+    # adjust emitted time to include microseconds
+    def formatTime(self, record, datefmt):
+        s, xs = divmod(record.created, 1)
+        us = int(round(xs*1e6))
+        t = time.strftime(datefmt, self.converter(s))
+        t += ".%06d" % us
+        return t

wcfs/internal/os.py
@@ -1,0 +1,29 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2024-2025  Nexedi SA and Contributors.
+#                          Kirill Smelkov <kirr@nexedi.com>
+#
+# This program is free software: you can Use, Study, Modify and Redistribute
+# it under the terms of the GNU General Public License version 3, or (at your
+# option) any later version, as published by the Free Software Foundation.
+#
+# You can also Link and Combine this program with other software covered by
+# the terms of any of the Free Software licenses or any of the Open Source
+# Initiative approved licenses and Convey the resulting work. Corresponding
+# source of such a combination shall include the source code for all other
+# software used.
+#
+# This program is distributed WITHOUT ANY WARRANTY; without even the implied
+# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+# See COPYING file for full licensing terms.
+# See https://www.nexedi.com/licensing for rationale and options.
+
+"""Package wcfs.internal.os complements operating system facilities provided by
+standard package os.
+
+- gettid returns OS-level identifier of current thread.
+"""
+
+from __future__ import print_function, absolute_import
+
+from wendelin.wcfs.internal._os import gettid

commit 3cb3872fd1e2435a1d9d8ad995ce45850f7bf4ec

wcfs: tests: Test WCFS CLI
https://lab.nexedi.com/nexedi/wendelin.core/-/merge_requests/38 attempts
to fix bugs introduced with https://lab.nexedi.com/nexedi/wendelin.core/-/commit/e51bef0d26ebaa32d81f7a92370d3f3f86521e6a.
We didn't see these bugs in test results, because the WCFS CLI codepath
is not covered in our tests. This patch adds coverage of this codepath
to increase the likelihood that issues are quickly detected.

wcfs/wcfs_test.py
@@ -2015,6 +2015,38 @@ def test_wcfs_crash_old_data():
     wl2.watch(zf, at2, {0:at1})
 
 
+# Verify WCFS CLI works as expected
+def test_wcfs_CLI(monkeypatch):
+    zurl = "file://abc"
+    optv = "-arg0"
+
+    cmd_list = []
+    def test(with_options):
+        def _(func):
+            cmd = func.__name__
+            monkeypatch.setattr(wcfs, cmd, func)
+            cmd_list.append((cmd, with_options))
+        return _
+
+    @test(1)
+    def serve(u, o, **kwargs):
+        assert u == zurl
+        assert o == (optv,)
+
+    @test(0)
+    def status(u):
+        assert u == zurl
+
+    @test(0)
+    def stop(u):
+        assert u == zurl
+
+    for cmd, opt in cmd_list:
+        opt = (optv,) if opt else ()
+        sys.argv = ("python", cmd) + opt + (zurl,)
+        wcfs.main()
+
+
 # ---- misc ---
 
 # tidtime converts tid to transaction commit time.